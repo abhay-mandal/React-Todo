f30ad7e6eba10215a44c3b7c593db960822ae4fa
Abhay Kumar Mandal <abhay.kumar@algonomy.com>
2024-01-02 14:11:12 +0530

    first commit

---
+++ src/todos/todo.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+import { useEffect, useState } from "react";
+
+function Todo() {
+
+  const [Todo, setTodo] = useState([]);
+
+  useEffect(()=>{
+    
+  });
+
+  
+
+    return (
+      <div className="todos">
+
+      </div>
+    );
+  }
+  
+  export default Todo;
